7114b8ff5b7151c471dff7846dcaa59fc8120d15
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-06-26 17:05:13 +0200

    Better explaination for the fail function

--- path/to/script.js
+++ path/to/script.js
@@ -1,2 +1,11 @@
-// Tell the App that we finished and didn't download anything
+/*
+ * Tell the App that we failed to download anything
+ *
+ * This stops the execution of the script and loads an empty page.
+ * 
+ * Always tell the App call this function when you don't call save()
+ * or calling save() fails.
+ */
+
+// Takes one argument: a string that describes the reason why the download failed
 fail('Currently does nothing');
