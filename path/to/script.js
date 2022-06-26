6b9b1c866e13f2da17e47b63f9f54f49bf022799
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-06-26 17:06:12 +0200

    Add information about the environment

--- path/to/script.js
+++ path/to/script.js
@@ -1,3 +1,6 @@
+// Because the App uses WebKit all JavaScript features from Safari are available.
+// This script is executed at "document end" -> when the DOM is fully loaded
+
 /*
  * Tell the App that we failed to download anything
  *
